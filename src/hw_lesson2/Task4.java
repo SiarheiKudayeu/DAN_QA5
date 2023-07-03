17dcf27fdea44dce1863c1970467741ee41b3751
Siarhei <greysiarhei@gmail.com>
2023-07-03 18:37:05 +0300

    commit1

---
+++ src/hw_lesson2/Task4.java
@@ -0,0 +1,19 @@
+package hw_lesson2;
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println("dvlkndasvlndsaklv");
+        System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");
+
+
+        System.out.println("dvlkndasvlndsaklv");
+        System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");System.out.println("dvlkndasvlndsaklv");
+
+
+
+
+
+
+
+    }
+}
---
+++ src/hw_lesson2/Task5.java
@@ -0,0 +1,7 @@
+package hw_lesson2;
+
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("Excellent");
+    }
+}
